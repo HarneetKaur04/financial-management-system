ea2d6a3011a2c4266205899c8eaefdc0b947cbca
Harneet Kaur <harneetsaini40@gmail.com>
2024-05-08 10:43:12 -0700

    added how it works and added a video there

--- client/src/pages/HowItWorks.js
+++ client/src/pages/HowItWorks.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./HowItWorks.css"; // Import CSS file for component-specific styles
 
 function HowItWorks() {
   return (
     <div className="top-container-how-it-works">
-        <h2>Add a Working Video Here</h2>
+        <h2>Watch how to navigate through the application</h2>
+        <video className="video-player" controls>
+          <source src="/assets/Full_video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
     </div>
   );
 }
